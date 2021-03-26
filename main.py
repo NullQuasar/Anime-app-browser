33bc2628eff7c7db2a1bba91def7b2c3583a06ea
NullQuasar <felipemejia816@gmail.com>
2021-03-26 17:47:54 -0500

    Re structured main.py code to OOP

--- main.py
+++ main.py
@@ -1,71 +1,44 @@
-import os, sys, re
+""" GUI modules """
+from PyQt5.QtWidgets import QMessageBox, QFileDialog
+from PyQt5.QtGui import QBrush, QImage, QPalette
+from PyQt5.QtCore import QSize
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+
+""" Scrapping """
 import requests
 
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from PyQt5.QtCore import QSize
-from PyQt5.QtGui import QBrush, QImage, QPalette
-from PyQt5.QtWidgets import QMessageBox, QFileDialog
+""" Useful modules """
+import os
+import sys
+import re
+
+#sys.stderr = open('error_log.txt', 'a') # set stderr custom output file
 
 
-#sys.stderr = open('error_log.txt', 'a') set stderr custom output file
-
+""" Class for user interface """
 class MainWindow(QtWidgets.QMainWindow):
     resized = QtCore.pyqtSignal()
-    def __init__(self):
-     
-        super(MainWindow, self).__init__()
-        self.currentPath = os.getcwd().replace('\\', '/')
-        self.backgrounds_path = self.currentPath + "/AnimeBrowserImages/"
 
+    def __init__(self):
+
+        super(MainWindow, self).__init__()
+
+        # Current path variable
+        self.currentPath = os.getcwd().replace('\\', '/')
         uic.loadUi(self.currentPath + '/AnimeBrowser.ui', self)
 
-        self.anime = self.SearchInput.text()
-        self.cap = ''
-        self.urls = []
-        nums = re.findall(r'\d+', self.anime)
-
-        if len(nums) > 0:
-            self.cap = nums[len(nums)-1]
-
-        
-        self.animeflvOp.stateChanged.connect(self.animeflvChecked)
-        self.animefenixOp.stateChanged.connect(self.animefenixChecked)
-        self.tioanimeOp.stateChanged.connect(self.tioanimeChecked)
-        self.monoschinosOp.stateChanged.connect(self.monoschinosChecked)
-
-        self.searchButton.clicked.connect(self.setQuery)
         self.resized.connect(self.resizeWindowAction)
-        self.change_bg.installEventFilter(self)
 
-        # Set background image:
-        self.make_background_folder()
-        self.image_index = 0
-        self.currentBackground = self.backgrounds_path + 'background4.jpg'
-        self.set_background()
-
-        self.checkedOps = set()  
-        self.openInBrowser()
-
+        # Useless for the current code
         """
         if True:
             import threading
             event = threading.Event()
             event.set()
-            thread = threading.Thread(target=self.variantBackground, args=(event,))
+            thread = threading.Thread(
+                target=self.variantBackground, args=(event,))
             thread.start()
         """
-            
-
-    def make_background_folder(self):
-        self.files = os.listdir(self.backgrounds_path)
-        # There is an error, rename files even while they are already named, and the order make the program to delete some files in the process
-        images = len(self.files)
-        for x in range(images):
-            c_image = self.backgrounds_path + self.files[x]
-            
-            if not self.files[x].startswith('background'):
-                os.system('mv {} {}background{}.{}'.format(c_image, self.backgrounds_path, str(x+1), c_image[c_image.rfind('.')+1:]))
-
 
     def resizeEvent(self, event):
         self.resized.emit()
@@ -83,19 +56,74 @@ class MainWindow(QtWidgets.QMainWindow):
         self.move(qr.topLeft())
 
 
+# =======================================================================================================
+
+""" Class with functions to scrape the websites """
+class GetAnime(MainWindow):
+    def __init__(self):
+        super(GetAnime, self).__init__()
+
+        # Backgrounds path
+        self.backgrounds_path = self.currentPath + "/AnimeBrowserImages/"
+
+        # Set basic variables needed for search
+        self.anime = self.SearchInput.text()
+        self.cap = ''
+        self.urls = []
+        nums = re.findall(r'\d+', self.anime)
+        self.checkedOps = set()  # Checked websites to search
+
+        # Check if is there any number to search by episode
+        if len(nums) > 0:
+            self.cap = nums[len(nums)-1]
+
+        # Choose websites for search anime
+        self.animeflvOp.stateChanged.connect(self.animeflvChecked)
+        self.animefenixOp.stateChanged.connect(self.animefenixChecked)
+        self.tioanimeOp.stateChanged.connect(self.tioanimeChecked)
+        self.monoschinosOp.stateChanged.connect(self.monoschinosChecked)
+
+        self.searchButton.clicked.connect(self.set_query)
+        self.change_bg.installEventFilter(self)
+
+        # Set background image:
+        self.make_background_folder()
+        self.image_index = 0
+        self.currentBackground = self.backgrounds_path + 'background4.jpg'
+        self.set_background()
+
+        # Check if open-in-browser is activated
+        self.open_in_browser()
+
+
+    def make_background_folder(self):
+        self.files = os.listdir(self.backgrounds_path)
+        # There is an error, rename files even while they are already named, and the order make the program to delete some files in the process
+        images = len(self.files)
+        for x in range(images):
+            c_image = self.backgrounds_path + self.files[x]
+
+            if not self.files[x].startswith('background'):
+                os.system('mv {} {}background{}.{}'.format(
+                    c_image, self.backgrounds_path, str(x+1), c_image[c_image.rfind('.')+1:]))
+
+
     def set_background(self, dx=1126, dy=704):
         oImage = QImage(self.currentBackground)
-        sImage = oImage.scaled(QSize(dx, dy))                   # resize Image to widgets size
+
+        # resize Image to widgets size
+        sImage = oImage.scaled(QSize(dx, dy))
         palette = QPalette()
-        palette.setBrush(QPalette.Window, QBrush(sImage))                        
+        palette.setBrush(QPalette.Window, QBrush(sImage))
         self.setPalette(palette)
 
 
     def animeflvChecked(self):
-        if self.animeflvOp.isChecked(): 
+        if self.animeflvOp.isChecked():
             self.checkedOps.add('https://www3.animeflv.net')
         else:
             self.checkedOps.remove('https://www3.animeflv.net')
+
 
     def animefenixChecked(self):
         if self.animefenixOp.isChecked():
@@ -103,11 +131,13 @@ class MainWindow(QtWidgets.QMainWindow):
         else:
             self.checkedOps.remove('https://www.animefenix.com')
 
+
     def tioanimeChecked(self):
         if self.tioanimeOp.isChecked():
             self.checkedOps.add('https://tioanime.com')
         else:
             self.checkedOps.remove('https://tioanime.com')
+
 
     def monoschinosChecked(self):
         if self.monoschinosOp.isChecked():
@@ -118,12 +148,13 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def typeOfSearch(self):
         if len(self.checkedOps) > 0:
-            if self.isEpisode():
+            if self.is_episode():
                 for p in self.checkedOps:
-                    self.SearchByEpisode(p)
+                    self.search_by_episode(p)
             else:
                 for p in self.checkedOps:
-                    self.SearchAnime(p)
+                    self.search_anime(p)
+                    print(p)
         else:
             msg = QMessageBox()
             msg.setWindowTitle('Platform!')
@@ -133,34 +164,29 @@ class MainWindow(QtWidgets.QMainWindow):
             x = msg.exec_()
 
 
-    def openInBrowser(self):
-        # Open links in browser
+    # Open links in browser
+    def open_in_browser(self):
         if self.openBrowser.isChecked():
             for p in self.urls:
                 os.system('start ' + p)
                 print('Open in browser > ' + p)
 
 
-    def clearList(self):
-        self.urls.clear()
-        self.Results.clear()
-        self.cap = ''
-
-
-    def remove_special_chars(self):
-        self.anime = re.sub('[^A-Za-z0-9, ]+', ' ', self.anime)
-        self.anime = re.sub(' +', ' ', self.anime)
-
-
-    def setQuery(self):
+    def set_query(self):
         print('Debug! ', self.checkedOps)
-        self.clearList()
+
+        def clear_list():
+            self.urls.clear()
+            self.Results.clear()
+            self.cap = ''
+
+        clear_list()
 
         self.anime = self.SearchInput.text()
-        self.remove_special_chars()
-        
+        self.anime = re.sub('[^A-Za-z0-9 ]+', ' ', self.anime)
+        self.anime = re.sub(' +', ' ', self.anime)
         print(self.anime)
-            
+
         # Verify input
         if self.anime == '':
             msg = QMessageBox()
@@ -173,46 +199,43 @@ class MainWindow(QtWidgets.QMainWindow):
         else:
             self.SearchInput.setText(self.anime.title())
 
-            def setCap(name):
+            def set_cap(name):
                 nums = re.findall(r'\d+', name)
                 if len(nums) > 0 and not name.startswith(''.join(nums)):
                     self.cap = nums[len(nums)-1]
-
 
             # Verify if input is a list
             if ',' in self.anime:
                 self.anime = self.anime.split(',')
                 for a in self.anime:
-                    setCap(a)
+                    set_cap(a)
                     self.anime = a
                     self.typeOfSearch()
-                    self.openInBrowser()
-                    #self.clearList()
-                    
+                    self.open_in_browser()
+                    clear_list()
 
             else:
-                setCap(self.anime)
+                set_cap(self.anime)
                 self.typeOfSearch()
-                self.openInBrowser()
+                self.open_in_browser()
 
 
-    def isEpisode(self):
-        # Determine type of search (query or episode)
+    def is_episode(self):
+        # Determinar tipo de busqueda (consulta o capitulo)
         if len(self.cap) == 0:
             return False
         else:
             return True
 
 
-
     # If the self.anime is not found it probes some common parameters like tv and hd
-    def ProbeOtherParameters(self, url, p):
+    def probe_parameters(self, url, p):
         parameters = ['tv', 'hd', 'latino']
 
         index = url.find('-' + self.cap)
         found = False
         for i in parameters:
-            
+
             newQuery = url[:index] + '-' + i + url[index:]
             print('Trying ' + newQuery)
 
@@ -221,13 +244,13 @@ class MainWindow(QtWidgets.QMainWindow):
                 print('Trying other parameters...')
 
             else:
-                print('Encontrado: ' + newQuery)
+                print('Found: ' + newQuery)
                 self.Results.addItem(QtWidgets.QListWidgetItem(newQuery))
                 self.urls.append(newQuery)
                 print('Added to the list.')
                 found = True
                 return
-        
+
         if not found:
             print('Anime or anime episode not found')
             msg = QMessageBox()
@@ -238,24 +261,21 @@ class MainWindow(QtWidgets.QMainWindow):
             x = msg.exec_()
 
 
-
-
-    def SearchAnime(self, platform):
-        #query = urllib.parse.quote(query) Dont work
+    def search_anime(self, platform):
+        # query = urllib.parse.quote(query) Dont work
         query = self.anime.replace(' ', '+')
 
         if platform == 'https://www3.animeflv.net':
-            param = '/browse?q=' # Param for animeflv
+            param = '/browse?q='  # Param for animeflv
 
-        elif platform == 'https://www.animefenix.com': # Param for animefenix
+        elif platform == 'https://www.animefenix.com':  # Param for animefenix
             param = '/animes?q='
 
-        elif platform == 'https://tioanime.com': # Param for tioanime
+        elif platform == 'https://tioanime.com':  # Param for tioanime
             param = '/directorio?q='
 
-        elif platform == 'https://monoschinos2.com': # Param for monoschinos
+        elif platform == 'https://monoschinos2.com':  # Param for monoschinos
             param = '/search?q='
-
 
         url = platform + param + query
 
@@ -264,24 +284,23 @@ class MainWindow(QtWidgets.QMainWindow):
         print('Added to the list.')
 
 
-
-    def SearchByEpisode(self, platform):
+    def search_by_episode(self, platform):
         query = self.anime.replace(' ', '-').lower()
         url = platform + '/ver/' + query
 
-        print(requests.get(url).status_code) # Debug
+        print(requests.get(url).status_code)  # Debug
         if requests.get(url).status_code == 404:
             print('Page not found.')
             print('Trying other parameters...')
-            self.ProbeOtherParameters(url, platform)
-            
+            self.probe_parameters(url, platform)
+
         else:
             self.Results.addItem(QtWidgets.QListWidgetItem(url))
             self.urls.append(url)
             print('Added to the list.')
 
 
-
+    # In development
     """ def variantBackground(self, event):
         from time import sleep
 
@@ -295,7 +314,6 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.currentPath = path + image
                 self.resizeWindowAction()
                 event.wait(sleepTime) """
-                
 
 
     def prev_background(self):
@@ -303,19 +321,24 @@ class MainWindow(QtWidgets.QMainWindow):
         if self.image_index <= 0:
             self.image_index = len(self.files) - 1
 
-        self.currentBackground = self.backgrounds_path + self.files[self.image_index]
+        self.currentBackground = self.backgrounds_path + \
+            self.files[self.image_index]
         self.set_background()
+
 
     def next_background(self):
         self.image_index += 1
         if self.image_index >= len(self.files):
             self.image_index = 0
 
-        self.currentBackground = self.backgrounds_path + self.files[self.image_index]
+        self.currentBackground = self.backgrounds_path + \
+            self.files[self.image_index]
         self.set_background()
 
+
     def custom_background(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', self.currentPath, 'Images (*.jpg *.jpeg *.png *.gif *.jfif *.bmp)')
+        fname = QFileDialog.getOpenFileName(
+            self, 'Open file', self.currentPath, 'Images (*.jpg *.jpeg *.png *.gif *.jfif *.bmp)')
 
         if os.path.isfile(fname[0]):
             self.currentBackground = fname[0]
@@ -342,10 +365,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.resizeWindowAction()
         return QtCore.QObject.event(obj, event)
 
-            
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
+    window = GetAnime()
     window.show()
     sys.exit(app.exec_())
